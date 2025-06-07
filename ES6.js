0bc725ffa7df14e0ca34e4cc5e5ea8030193e732
strzcy <desipuspita46832@gmail.com>
2025-06-07 08:40:06 +0700

    omaigotnes

--- ES6.js
+++ ES6.js
@@ -1,3 +1,6 @@
-const sumThreeNumbers = (a,b,c) => a+b+c;
+const sumThreeNumbers = (a,b,c) => a+b+c; 
+// arrow function
 const threeNumbers = [2,5,7];
 console.log(sumThreeNumbers(...threeNumbers));
+// spread syntax
+
